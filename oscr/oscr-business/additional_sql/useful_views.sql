633cde7ec36687950795ee48e0a5a8951eeb055c
Bernhard Streit <Bernhard.Streit@gmail.com>
2014-07-06 21:39:04 +0200

    More Views

--- oscr/oscr-business/additional_sql/useful_views.sql
+++ oscr/oscr-business/additional_sql/useful_views.sql
@@ -36,7 +36,7 @@ CREATE VIEW BILLS_SUMMED_UP_BY_DAY AS
 	SELECT 
 		CAST(billclosed AS DATE) AS date, 
 		SUM(total) AS total, 
-		pricecurrency  
+		pricecurrency
 	
 	FROM 
 		BILLS_SUMMED_UP 
@@ -84,152 +84,230 @@ CREATE VIEW STAT_BILLITEMS_BY_HOUR_AND_NAME AS
 		name;
 
 CREATE VIEW BILLS_SUMMED_UP_BY_DAY_THIS_MONTH AS
-        SELECT
-                CAST(billclosed AS DATE) AS date,
-                SUM(total) AS total,
-                pricecurrency
+	SELECT
+		CAST(billclosed AS DATE) AS date,
+		SUM(total) AS total,
+		pricecurrency
 
-        FROM
-                BILLS_SUMMED_UP
+	FROM
+		BILLS_SUMMED_UP
 
-        WHERE
-		billopened >= formatdatetime(current_timestamp, 'YYYY-MM-01')
-        GROUP BY
-                pricecurrency, date;
+	WHERE
+			billopened >= formatdatetime(current_timestamp, 'YYYY-MM-01')
+	GROUP BY
+		pricecurrency, date;
 
 CREATE VIEW BILLS_SUMMED_UP_BY_DAY_LAST_MONTH AS
-        SELECT
-                CAST(billclosed AS DATE) AS date,
-                SUM(total) AS total,
-                pricecurrency
+	SELECT
+		CAST(billclosed AS DATE) AS date,
+		SUM(total) AS total,
+		pricecurrency
 
-        FROM
-                BILLS_SUMMED_UP
+	FROM
+		BILLS_SUMMED_UP
 
-        WHERE
-		billopened >= formatdatetime(dateadd('Month', -1, current_timestamp), 'YYYY-MM-01')
-		AND billopened < formatdatetime(current_timestamp, 'YYYY-MM-01')
+	WHERE
+			billopened >= formatdatetime(dateadd('Month', -1, current_timestamp), 'YYYY-MM-01')
+			AND billopened < formatdatetime(current_timestamp, 'YYYY-MM-01')
 
-        GROUP BY
-                pricecurrency, date;
+	GROUP BY
+		pricecurrency, date;
 
 
 CREATE VIEW STAT_BILLITEMS_PER_HOUR_THIS_MONTH AS
-        SELECT
-                SUBSTRING(billopened, 12, 2) AS hour,
-                COUNT(*)
-        FROM
-                BILLS_WITH_PRODUCTS
+	SELECT
+		SUBSTRING(billopened, 12, 2) AS hour,
+		COUNT(*)
+	FROM
+		BILLS_WITH_PRODUCTS
 	WHERE
 		billopened >= formatdatetime(current_timestamp, 'YYYY-MM-01')
-        GROUP BY
-                hour
-        ORDER BY
-                hour;
+	GROUP BY
+		hour
+	ORDER BY
+		hour;
 
 CREATE VIEW STAT_BILLITEMS_PER_HOUR_LAST_MONTH AS
-        SELECT
-                SUBSTRING(billopened, 12, 2) AS hour,
-                COUNT(*)
-        FROM
-                BILLS_WITH_PRODUCTS
-        WHERE
-                billopened >= formatdatetime(dateadd('Month', -1, current_timestamp), 'YYYY-MM-01')
-                AND billopened < formatdatetime(current_timestamp, 'YYYY-MM-01')
-        GROUP BY
-                hour
-        ORDER BY
-                hour;
+	SELECT
+		SUBSTRING(billopened, 12, 2) AS hour,
+		COUNT(*)
+	FROM
+		BILLS_WITH_PRODUCTS
+	WHERE
+		billopened >= FORMATDATETIME(DATEADD('Month', -1, CURRENT_TIMESTAMP), 'YYYY-MM-01') AND
+		billopened < FORMATDATETIME(CURRENT_TIMESTAMP, 'YYYY-MM-01')
+	GROUP BY
+		hour
+	ORDER BY
+		hour;
 
 
 CREATE VIEW BILLS_TOTAL_THIS_MONTH AS
-        SELECT
-                formatdatetime(current_timestamp, 'YYYY-MM'),
-                SUM(total) AS total,
-                pricecurrency
+	SELECT
+		formatdatetime(current_timestamp, 'YYYY-MM'),
+		SUM(total) AS total,
+		pricecurrency
 
-        FROM
-                BILLS_SUMMED_UP
+	FROM
+		BILLS_SUMMED_UP
 
-        WHERE
-                billopened >= formatdatetime(current_timestamp, 'YYYY-MM-01')
-        GROUP BY
-                pricecurrency;
+	WHERE
+		billopened >= formatdatetime(current_timestamp, 'YYYY-MM-01')
+	GROUP BY
+		pricecurrency;
 
 CREATE VIEW BILLS_TOTAL_LAST_MONTH AS
-        SELECT
-                formatdatetime(current_timestamp, 'YYYY-MM'),
-                SUM(total) AS total,
-                pricecurrency
+	SELECT
+		FORMATDATETIME(CURRENT_TIMESTAMP, 'YYYY-MM'),
+		SUM(total) AS total,
+		pricecurrency
 
-        FROM
-                BILLS_SUMMED_UP
-        WHERE
-                billopened >= formatdatetime(dateadd('Month', -1, current_timestamp), 'YYYY-MM-01')
-                AND billopened < formatdatetime(current_timestamp, 'YYYY-MM-01')
+	FROM
+		BILLS_SUMMED_UP
+	WHERE
+		billopened >= FORMATDATETIME(DATEADD('Month', -1, CURRENT_TIMESTAMP), 'YYYY-MM-01') AND
+		billopened < FORMATDATETIME(CURRENT_TIMESTAMP, 'YYYY-MM-01')
 
-        GROUP BY
-                pricecurrency;
+	GROUP BY
+		pricecurrency;
 
 
 CREATE VIEW PIVOTSTAT_BILLITEMS_PER_HOUR_THIS_MONTH AS
-        SELECT
-
-              replace(
-              replace(
-              replace(
-              replace(
-              replace(
-              replace(
-              replace(
-formatdatetime(billopened, 'E'), 
-  'Mo', '1 Mo'), 
-  'Di', '2 Di'), 
-  'Mi', '3 Mi'), 
-  'Do', '4 Do'), 
-  'Fr', '5 Fr'), 
-  'Sa', '6 Sa'), 
-  'So', '7 So')
-                    AS weekday,
-                SUBSTRING(billopened, 12, 2) AS hour,
-                COUNT(*)
-        FROM
-                BILLS_WITH_PRODUCTS
+	SELECT
+		REPLACE(
+			REPLACE(
+				REPLACE(
+					REPLACE(
+						REPLACE(
+							REPLACE(
+								REPLACE(
+									FORMATDATETIME(billopened, 'E'), 'Mo', '1 Mo'), 
+								'Di', '2 Di'), 
+							'Mi', '3 Mi'), 
+						'Do', '4 Do'), 
+					'Fr', '5 Fr'), 
+				'Sa', '6 Sa'), 
+			'So', '7 So') AS weekday,	
+		SUBSTRING(billopened, 12, 2) AS hour,
+		COUNT(*)
+	FROM
+		BILLS_WITH_PRODUCTS
 	WHERE
-		billopened >= formatdatetime(current_timestamp, 'YYYY-MM-01')
-        GROUP BY
-                weekday, hour
-        ORDER BY
-                hour;
+		billopened >= FORMATDATETIME(CURRENT_TIMESTAMP, 'YYYY-MM-01')
+	GROUP BY
+		weekday, hour
+	ORDER BY
+		hour;
  
 
 CREATE VIEW PIVOTSTAT_BILLITEMS_PER_HOUR_LAST_MONTH AS
-        SELECT
+SELECT
+		REPLACE(
+			REPLACE(
+				REPLACE(
+					REPLACE(
+						REPLACE(
+							REPLACE(
+								REPLACE(
+									FORMATDATETIME(billopened, 'E'), 'Mo', '1 Mo'), 
+								'Di', '2 Di'), 
+							'Mi', '3 Mi'), 
+						'Do', '4 Do'), 
+					'Fr', '5 Fr'), 
+				'Sa', '6 Sa'), 
+			'So', '7 So') AS weekday,
+			SUBSTRING(billopened, 12, 2) AS hour,
+			COUNT(*)
+	FROM
+		BILLS_WITH_PRODUCTS
+	WHERE
+		billopened >= FORMATDATETIME(DATEADD('Month', -1, CURRENT_TIMESTAMP), 'YYYY-MM-01') AND
+		billopened < FORMATDATETIME(CURRENT_TIMESTAMP, 'YYYY-MM-01')
+	GROUP BY
+		weekday, hour
+	ORDER BY
+		hour;
 
-              replace(
-              replace(
-              replace(
-              replace(
-              replace(
-              replace(
-              replace(
-formatdatetime(billopened, 'E'), 
-  'Mo', '1 Mo'), 
-  'Di', '2 Di'), 
-  'Mi', '3 Mi'), 
-  'Do', '4 Do'), 
-  'Fr', '5 Fr'), 
-  'Sa', '6 Sa'), 
-  'So', '7 So')
-                    AS weekday,
-                SUBSTRING(billopened, 12, 2) AS hour,
-                COUNT(*)
-        FROM
-                BILLS_WITH_PRODUCTS
-        WHERE
-                billopened >= formatdatetime(dateadd('Month', -1, current_timestamp), 'YYYY-MM-01')
-                AND billopened < formatdatetime(current_timestamp, 'YYYY-MM-01')
-        GROUP BY
-                weekday, hour
-        ORDER BY
-                hour;
+CREATE VIEW AVG_TOTALS_THIS_MONTH AS
+	SELECT
+		REPLACE(
+			REPLACE(
+				REPLACE(
+					REPLACE(
+						REPLACE(
+							REPLACE(
+								REPLACE(
+									FORMATDATETIME(billopened, 'E'), 'Mo', '1 Mo'), 
+								'Di', '2 Di'), 
+							'Mi', '3 Mi'), 
+						'Do', '4 Do'), 
+					'Fr', '5 Fr'), 
+				'Sa', '6 Sa'), 
+			'So', '7 So') AS weekday,		
+		SUM(total) AS total,
+		AVG(total) as avg_total,
+		pricecurrency
+		
+	FROM
+		(
+			select
+				SUBSTR(billopened, 0, 10) billopened, 
+				SUM(total) total, 
+				pricecurrency
+			from
+				BILLS_SUMMED_UP
+			WHERE
+				billopened >= FORMATDATETIME(CURRENT_TIMESTAMP, 'YYYY-MM-01')
+			GROUP BY 
+				pricecurrency, billopened
+		)
+
+	GROUP BY
+		pricecurrency, 
+		weekday
+
+	ORDER BY 
+		weekday;
+
+create view avg_totals_last_month as 
+	SELECT
+		REPLACE(
+			REPLACE(
+				REPLACE(
+					REPLACE(
+						REPLACE(
+							REPLACE(
+								REPLACE(
+									FORMATDATETIME(billopened, 'E'), 'Mo', '1 Mo'), 
+								'Di', '2 Di'), 
+							'Mi', '3 Mi'), 
+						'Do', '4 Do'), 
+					'Fr', '5 Fr'), 
+				'Sa', '6 Sa'), 
+			'So', '7 So') AS weekday,	
+		SUM(total) AS total,
+		AVG(total) as avg_total,
+		pricecurrency
+		
+	FROM
+		(
+			SELECT
+				SUBSTR(billopened, 0, 10) billopened, 
+				SUM(total) total, 
+				pricecurrency
+			FROM
+				BILLS_SUMMED_UP
+			WHERE
+				billopened >= FORMATDATETIME(DATEADD('Month', -1, CURRENT_TIMESTAMP), 'YYYY-MM-01') AND
+				billopened < FORMATDATETIME(CURRENT_TIMESTAMP, 'YYYY-MM-01')
+			GROUP BY 
+				pricecurrency, billopened
+		)
+
+	GROUP BY
+		pricecurrency, 
+		weekday
+
+	order by 
+		weekday;
+		
