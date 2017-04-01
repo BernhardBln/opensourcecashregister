089d13e281282c60d3b876a6a9f6193bad2319f5
Bernhard Streit <Bernhard.Streit@gmail.com>
2017-04-01 18:42:44 +0200

    Fixed money test

--- oscr/oscr-business/src/test/java/de/bstreit/java/oscr/business/base/finance/money/MoneyTest.java
+++ oscr/oscr-business/src/test/java/de/bstreit/java/oscr/business/base/finance/money/MoneyTest.java
@@ -1,10 +1,10 @@
 package de.bstreit.java.oscr.business.base.finance.money;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Created by bernhard on 24.03.17.
@@ -12,44 +12,45 @@ import static org.junit.Assert.*;
 public class MoneyTest {
 
 
-    @Test
-    public void round_noRounding() throws Exception {
-        assertEquals(bigInt("1.00"), new Money("1", "EUR").round().getAmount());
-        assertEquals(bigInt("1.00"), new Money("1.0", "EUR").round().getAmount());
-        assertEquals(bigInt("1.00"), new Money("1.00", "EUR").round().getAmount());
+  @Test
+  public void round_noRounding() throws Exception {
+    assertEquals(bigInt("1.00"), new Money("1", "EUR").roundToTenCents().getAmount());
+    assertEquals(bigInt("1.00"), new Money("1.0", "EUR").roundToTenCents().getAmount());
+    assertEquals(bigInt("1.00"), new Money("1.00", "EUR").roundToTenCents().getAmount());
 
-        assertEquals(bigInt("1.10"), new Money("1.1", "EUR").round().getAmount());
-        assertEquals(bigInt("1.10"), new Money("1.10", "EUR").round().getAmount());
+    assertEquals(bigInt("1.10"), new Money("1.1", "EUR").roundToTenCents().getAmount());
+    assertEquals(bigInt("1.10"), new Money("1.10", "EUR").roundToTenCents().getAmount());
 
-        assertEquals(bigInt("1.10"), new Money("1.1", "EUR").round().getAmount());
-        assertEquals(bigInt("1.10"), new Money("1.10", "EUR").round().getAmount());
+    assertEquals(bigInt("1.10"), new Money("1.1", "EUR").roundToTenCents().getAmount());
+    assertEquals(bigInt("1.10"), new Money("1.10", "EUR").roundToTenCents().getAmount());
 
-        assertEquals(bigInt("1.50"), new Money("1.5", "EUR").round().getAmount());
-        assertEquals(bigInt("1.50"), new Money("1.50", "EUR").round().getAmount());
+    assertEquals(bigInt("1.50"), new Money("1.5", "EUR").roundToTenCents().getAmount());
+    assertEquals(bigInt("1.50"), new Money("1.50", "EUR").roundToTenCents().getAmount());
 
-        assertEquals(bigInt("1.90"), new Money("1.9", "EUR").round().getAmount());
-        assertEquals(bigInt("1.90"), new Money("1.90", "EUR").round().getAmount());
+    assertEquals(bigInt("1.90"), new Money("1.9", "EUR").roundToTenCents().getAmount());
+    assertEquals(bigInt("1.90"), new Money("1.90", "EUR").roundToTenCents().getAmount());
 
-    }
+  }
 
-    @Test
-    public void round_rounding() throws Exception {
-        assertEquals(bigInt("1.00"), new Money("1.001", "EUR").round().getAmount());
-        assertEquals(bigInt("1.00"), new Money("1.01", "EUR").round().getAmount());
-        assertEquals(bigInt("1.00"), new Money("1.04", "EUR").round().getAmount());
+  @Test
+  public void round_rounding() throws Exception {
+    assertEquals(bigInt("1.00"), new Money("1.001", "EUR").roundToTenCents().getAmount());
+    assertEquals(bigInt("1.00"), new Money("1.01", "EUR").roundToTenCents().getAmount());
+    assertEquals(bigInt("1.00"), new Money("1.04", "EUR").roundToTenCents().getAmount());
 
-        // This becomes 1.10, as 1.049 (when turned into Money) already is rounded to 1.05)
-        assertEquals(bigInt("1.10"), new Money("1.049", "EUR").round().getAmount());
+    // This becomes 1.10, as 1.049 (when turned into Money) already is rounded
+    // to 1.05)
+    assertEquals(bigInt("1.10"), new Money("1.049", "EUR").roundToTenCents().getAmount());
 
-        assertEquals(bigInt("1.10"), new Money("1.05", "EUR").round().getAmount());
-        assertEquals(bigInt("1.10"), new Money("1.051", "EUR").round().getAmount());
-        assertEquals(bigInt("1.10"), new Money("1.09", "EUR").round().getAmount());
-        assertEquals(bigInt("1.10"), new Money("1.099", "EUR").round().getAmount());
+    assertEquals(bigInt("1.10"), new Money("1.05", "EUR").roundToTenCents().getAmount());
+    assertEquals(bigInt("1.10"), new Money("1.051", "EUR").roundToTenCents().getAmount());
+    assertEquals(bigInt("1.10"), new Money("1.09", "EUR").roundToTenCents().getAmount());
+    assertEquals(bigInt("1.10"), new Money("1.099", "EUR").roundToTenCents().getAmount());
 
-    }
+  }
 
-    private BigDecimal bigInt(String i) {
-        return new BigDecimal(i);
-    }
+  private BigDecimal bigInt(String i) {
+    return new BigDecimal(i);
+  }
 
 }
